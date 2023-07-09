807a956462efe2bd84d95ad6b72139c686b94b1e
washedlih <khanglee11@gmail.com>
2023-07-09 18:34:45 -0400

    fix header onload

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-// window.onbeforeunload = function () {
-//   window.scrollTo(0, 0);
-// };
+window.onload = function () {
+  // add hidden class to all elements inside the header element
+  const headerChildren = document.querySelector('header').querySelectorAll('*');
+  headerChildren.forEach((child) => child.classList.add('hidden'));
+};
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      const children = entry.target.querySelectorAll("*");
-      children.forEach((child) => child.classList.add("hidden2"));
+      const children = entry.target.querySelectorAll('*');
+      children.forEach((child) => child.classList.add('hidden2'));
     }
   });
 });
 
-const element = document.getElementById("cta-section");
+const element = document.getElementById('cta-section');
 observer.observe(element);
 
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const nav = document.querySelector("nav");
-menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
-  menu.classList.toggle("open");
-  nav.classList.toggle("open");
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('nav');
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('open');
+  menu.classList.toggle('open');
+  nav.classList.toggle('open');
 });
-
